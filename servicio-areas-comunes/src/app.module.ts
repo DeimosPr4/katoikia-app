d470e57b66c96048cf359baa9193bd95542d97bb
Mariela <mbonilla.guti@gmail.com>
2022-06-30 19:43:44 -0600

    add microservice areas comunes

---
+++ servicio-areas-comunes/src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}

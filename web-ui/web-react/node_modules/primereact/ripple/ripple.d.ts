96b9196f900ee8d24ea3d36be7e4a2ebeecf1b0c
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-07-08 22:09:25 -0600

    remove node_modules directories with kondo

--- web-ui/web-react/node_modules/primereact/ripple/ripple.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-
-export interface RippleProps {}
-
-export declare class Ripple extends React.Component<RippleProps, any> { }
--- web-ui/web-react/node_modules/primereact/scrollpanel/scrollpanel.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export interface ScrollPanelProps {
-    id?: string;
-    style?: object;
-    className?: string;
-}
-
-export declare class ScrollPanel extends React.Component<ScrollPanelProps, any> { }

790f589abc4d3074eb9496f7abf4e4fb162bfbbb
Maria Sanchez <paosanchez30@hotmail.com>
2022-07-13 11:54:14 -0600

    rename template folder and created folder for own components

--- web-ui/web-react/src/components/LogIn.js
+++ web-ui/web-react/src/components/LogIn.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { InputText } from 'primereact/inputtext';
 
 const LogIn = () => {
 
-    return(
-        <div> 
-              {/* <span className="p-float-label">
-                        <InputText id="username" type="text" value={floatValue} onChange={(e) => setFloatValue(e.target.value)} />
-                        <label htmlFor="username">Username</label>
-                    </span> */}
+    return (
+        <div className="grid">
+            <div className="col-12">
+                <div className="card">
+                    <h5>Iniciar Sesión</h5>
+                    <div className="p-fluid formgrid grid">
+                        <div className="field col-12">
+                            <label htmlFor="nombre">Correo electrónico</label>
+                            <InputText id="nombre" type="text" />
+                        </div>
+                        <div className="field col-12 ">
+                            <label htmlFor="apellidos">Contraseña</label>
+                            <InputText id="apellidos" type="text" />
+                        </div>
+
+                        {/* <Button label="Registrar" onClick={registrarAdmin}></Button> */}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

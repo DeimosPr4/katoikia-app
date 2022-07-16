02b59e7fe488fd4224d25d92fc085dddd416ea7b
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-07-16 02:36:25 -0600

    remover información innecesaria para registro de inquilino

--- web-ui/web-react/src/components/Inquilinos.js
+++ web-ui/web-react/src/components/Inquilinos.js
@@ -7,12 +7,7 @@ import { Column } from 'primereact/column'
 const Inquilinos = () => {
   function registrarInquilino() {
     let data = {
-      dni: document.getElementById('identificacion').value,
-      name: document.getElementById('nombre').value,
-      last_name: document.getElementById('apellidos').value,
       email: document.getElementById('correo_electronico').value,
-      phone: document.getElementById('telefono').value,
-      password: document.getElementById('correo_electronico').value,
       user_type: '1', //1 es admin
       status: '1',
     }

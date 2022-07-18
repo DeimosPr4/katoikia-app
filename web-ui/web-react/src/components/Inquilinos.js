dacf7e2864689f0cb3e3daf68ff2de9f7a0c83fb
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-07-17 19:18:50 -0600

    remove unused imports

--- web-ui/web-react/src/components/Inquilinos.js
+++ web-ui/web-react/src/components/Inquilinos.js
@@ -1,6 +1,4 @@
-import { React, useEffect, useState } from 'react'
-import { InputText } from 'primereact/inputtext'
-import { Button } from 'primereact/button'
+import { React } from 'react'
 import { DataTable } from 'primereact/datatable'
 import { Column } from 'primereact/column'
 

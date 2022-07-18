48c271ff4c5b4363078b526e8796f3a6f505cee2
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-07-17 19:20:03 -0600

    add data table to list info

--- web-ui/web-react/src/components/Inquilinos.js
+++ web-ui/web-react/src/components/Inquilinos.js
@@ -10,4 +10,22 @@ const Inquilinos = () => {
       status: '1',
     }
   }
+
+  return (
+    <div className="grid">
+      <div className="col-12">
+        <div className="card">
+          <h5>Inquilinos</h5>
+          <DataTable value={listaInquilinos} scrollable scrollHeight="400px" scrollDirection="both" className="mt-3">
+            <Column field="name" header="Nombre" style={{ flexGrow: 1, flexBasis: '160px' }}></Column>
+            <Column field="last_name" header="Apellidos" style={{ flexGrow: 1, flexBasis: '160px' }}></Column>
+            <Column field="dni" header="Identificación" style={{ flexGrow: 1, flexBasis: '160px' }}></Column>
+            <Column field="email" header="Correo electrónico" style={{ flexGrow: 1, flexBasis: '160px' }}></Column>
+            <Column field="phone" header="Telefóno" style={{ flexGrow: 1, flexBasis: '180px' }}></Column>
+            <Column field="community_id" header="Comunidad" style={{ flexGrow: 1, flexBasis: '160px' }}></Column>
+          </DataTable>
+        </div>
+      </div>
+    </div>
+  )
 }

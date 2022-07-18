b1feb236b5307505115f5d5d248789e0dae604d8
Maria Sanchez <paosanchez30@hotmail.com>
2022-07-18 14:54:08 -0600

    mobile template modify

--- mobile-ui/App.tsx
+++ mobile-ui/App.tsx
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+//import { NavigationContainer } from "@react-navigation/native";
+import { Button } from './components';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
+
+      <Button>testing</Button>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
--- web-ui/web-react/src/components/LogIn.js
+++ web-ui/web-react/src/components/LogIn.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { InputText } from 'primereact/inputtext';
 
 const LogIn = () => {
 
     return(
         <div> 
-              {/* <span className="p-float-label">
-                        <InputText id="username" type="text" value={floatValue} onChange={(e) => setFloatValue(e.target.value)} />
+
+              <span className="p-float-label">
+                        <InputText id="username" type="text" />
                         <label htmlFor="username">Username</label>
-                    </span> */}
+                    </span>
         </div>
     )
+
+    //value={floatValue} onChange={(e) => setFloatValue(e.target.value)} 
 }
 
 export default LogIn

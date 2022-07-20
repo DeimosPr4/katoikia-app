8ddc277751677ad30fa5a460d13d2327233b3a36
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-07-19 19:21:37 -0600

    arreglar rendering de pagina de inquilinos

--- web-ui/web-react/src/components/Inquilinos.js
+++ web-ui/web-react/src/components/Inquilinos.js
@@ -1,5 +1,5 @@
 import { InputText } from 'primereact/inputtext'
-import { React } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Inquilinos = () => {
   function registrarInquilino() {

9e04a6e1c5786d5eff4ba49a12991f317c353a28
Mariela <mbonilla.guti@gmail.com>
2022-07-22 00:22:09 -0600

    delete function test send mail del service del api gateway

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -343,20 +343,5 @@ export class AppController {
     return this.appService.html(email, name);
   }
 
-    // #==== API Users
-    @Post('user/testSendMail')
-    testSendMail(
-      @Body('dni') dni: string,
-      @Body('name') name: string,
-      @Body('last_name') last_name: string,
-      @Body('email') email: string,
-      @Body('phone') phone: number,
-      @Body('password') password: string,
-      @Body('user_type') user_type: string,
-      @Body('status') status: string,
-      @Body('date_entry') date_entry: Date,
-    ) {
-      return this.appService.testSendMail(dni, name, last_name, email, phone, password,
-        user_type, status, date_entry);
-    }
+   
 }

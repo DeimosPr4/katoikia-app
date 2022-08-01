9dce98fc76d581d3b6b347bf5df1abb65137e317
Mariela <mbonilla.guti@gmail.com>
2022-07-31 23:49:05 -0600

    add areas comunes in app.js

---
+++ web-ui/web-react/src/components/AreasComunes.js
@@ -0,0 +1,12 @@
+import { Button } from 'primereact/button';
+import { Dropdown } from 'primereact/dropdown';
+import { InputText } from 'primereact/inputtext';
+import React, { useEffect, useState } from 'react';
+
+
+const AreasComunes = () => {
+
+};
+
+
+export default React.memo(AreasComunes);

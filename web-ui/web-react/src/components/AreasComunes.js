4a8effac6d7c2960f05dfa1f4a4b2e98a8505a12
Mariela <mbonilla.guti@gmail.com>
2022-07-31 23:52:01 -0600

    create consts e imports

--- web-ui/web-react/src/components/AreasComunes.js
+++ web-ui/web-react/src/components/AreasComunes.js
@@ -1,10 +1,48 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { Dropdown } from 'primereact/dropdown';
-import { InputText } from 'primereact/inputtext';
-import React, { useEffect, useState } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Toast } from 'primereact/toast';
+import { Dialog } from 'primereact/dialog';
+import { Toolbar } from 'primereact/toolbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUserAlt } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { faAt } from '@fortawesome/free-solid-svg-icons';
+import { faIdCardAlt } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
+import { faHomeAlt } from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames';
 
 
 const AreasComunes = () => {
+
+    let emptyCommonArea = {
+        _id: null,
+        dni: '',
+        name: '',
+        last_name: '',
+        email: '',
+        phone: '',
+        password: '',
+        confirmPassword: '',
+        community_id: '',
+        community_name: '',
+        user_type: '2',
+        date_entry: new Date(),
+        status: '1'
+    };
+
+    const [listaCommonAreas, setListaCommonAreas] = useState([]);
+    const [commonArea, setCommonArea] = useState(emptyCommonArea);
+    const [selectedAdminsCommunities, setSelectedAdminsCommunities] = useState(null);
+    const [deleteAdminCommunityDialog, setDeleteAdminCommunityDialog] = useState(false);
+    const [deleteAdminsCommunitiesDialog, setDeleteAdminsCommunitiesDialog] = useState(false);
+    const [submitted, setSubmitted] = useState(false);
+    const toast = useRef(null);
+    const dt = useRef(null);
 
 };
 

226e0b81a6781905d4118eb3c9d0a0f1927cef89
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-08-08 22:59:18 -0600

    add name and last_name props

--- web-ui/web-react/src/components/generic/InfoDialog.js
+++ web-ui/web-react/src/components/generic/InfoDialog.js
@@ -15,7 +15,29 @@ class InfoDialog extends React.Component {
         modal
         className='p-fluid'
         header='Información'
-      ></Dialog>
+      >
+        <div className='container text-center'>
+          <div className='row my-4'>
+            <div className='col-4 md:col-4'>
+              <p>Nombre</p>
+              <div className='p-0 col-2 md:col-2' style={{ margin: '0 auto' }}>
+                <div className='p-inputgroup align-items-center justify-content-evenly'>
+                  <i className='pi pi-user icon-khaki' />
+                  <p>{this.props.info.name}</p>
+                </div>
+              </div>
+            <div className='col-4 md:col-4'>
+              <p>Apellido(s)</p>
+              <div className='p-0 col-2 md:col-2' style={{ margin: '0 auto' }}>
+                <div className='p-inputgroup align-items-center justify-content-evenly'>
+                  <i className='pi pi-user icon-khaki' />
+                  <p>{this.props.info.last_name}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Dialog>
     )
   }
 }

0233e0c0b413857765edb23c2b0a986245ac5f5c
Maria Sanchez <paosanchez30@hotmail.com>
2022-08-18 22:30:47 -0600

    componentes de invitados

---
+++ mobile-ui/components/AgregarInvitados.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import {
+ 
+  Center,
+ 
+} from "native-base";
+
+
+export default function AgregarInvitados({navigation}) {
+  return (
+  
+      
+      <Center w="100%">
+       
+         
+      </Center>
+  );
+
+
+ 
+}
---
+++ mobile-ui/components/Invitados.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import {
+ 
+  Center,
+ 
+} from "native-base";
+
+
+export default function Invitados({navigation}) {
+  return (
+  
+      
+      <Center w="100%">
+       
+         
+      </Center>
+  );
+
+
+ 
+}

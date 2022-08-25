da98c3ab2621e7bbd777a2a194bd594e1fcf1ff4
Traym17 <51390112+Traym17@users.noreply.github.com>
2022-08-24 22:36:55 -0600

    Correcciones

--- mobile-ui/App.js
+++ mobile-ui/App.js
@@ -72,7 +72,7 @@ export default function App() {
       <UserContextProvider>
         <NavigationContainer>
           <Stack.Navigator initialRouteName="LogIn">
-            <Stack.Screen name="Inicio" component={Invitados} options={{
+            <Stack.Screen name="Inicio" component={LogIn} options={{
               headerStyle: {
                 backgroundColor: "#D7A86E"
               }
